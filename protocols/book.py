2a16308f497cbe54356e838aff4907045c8a378b
AshrayGattani <ashrayg5@gmail.com>
2023-12-27 12:37:06 +0530

    protos update

--- protocols/book.py
+++ protocols/book.py
@@ -1,43 +1,32 @@
 from uagents import Context, Model, Protocol
-from .query import ClothingArticle  # Import the ClothingArticle class from the previous code
-from typing import List
-class QueryClothingRequest(Model):
-    color: str
-    size: str
-    style: str
+from query import ClothingArticle  # Importing ClothingArticle class from query.py
 
-class QueryClothingResponse(Model):
-    articles: List[int]
+class BookClothingRequest(Model):
+    article_number: int
+    user_id: str  # Example: user identification
+    # Add other necessary fields for the booking request
 
-class GetTotalClothingQueries(Model):
-    pass
-
-class TotalClothingQueries(Model):
-    total_queries: int
+class BookClothingResponse(Model):
+    success: bool
 
 cloth_book_proto = Protocol()
 
-@cloth_book_proto.on_message(model=QueryClothingRequest, replies=QueryClothingResponse)
-async def handle_clothing_query_request(ctx: Context, sender: str, msg: QueryClothingRequest):
+@cloth_book_proto.on_message(model=BookClothingRequest, replies=BookClothingResponse)
+async def handle_book_clothing_request(ctx: Context, sender: str, msg: BookClothingRequest):
     articles = {
         int(num): ClothingArticle(**attributes)
         for num, attributes in ctx.storage._data.items()
         if isinstance(num, int)
     }
-    available_articles = []
-    for number, article in articles.items():
-        if (
-            article.color == msg.color
-            and article.size == msg.size
-            and article.style == msg.style
-        ):
-            available_articles.append(int(number))
-    ctx.logger.info(f"Query: {msg}. Available articles: {available_articles}.")
-    await ctx.send(sender, QueryClothingResponse(articles=available_articles))
-    total_clothing_queries = int(ctx.storage.get("total_clothing_queries") or 0)
-    ctx.storage.set("total_clothing_queries", total_clothing_queries + 1)
-
-@cloth_book_proto.on_query(model=GetTotalClothingQueries, replies=TotalClothingQueries)
-async def handle_get_total_clothing_queries(ctx: Context, sender: str, _msg: GetTotalClothingQueries):
-    total_clothing_queries = int(ctx.storage.get("total_clothing_queries") or 0)
-    await ctx.send(sender, TotalClothingQueries(total_queries=total_clothing_queries))
+    article = articles.get(msg.article_number)
+    
+    # Perform reservation logic based on the available articles
+    if article and article.available:  # Assuming there's an 'available' attribute in ClothingArticle
+        article.reserve_for_user(msg.user_id)  # Example method to reserve the article for a user
+        ctx.storage.set(msg.article_number, article.dict())
+        success = True
+    else:
+        success = False
+    
+    # Send the response
+    await ctx.send(sender, BookClothingResponse(success=success))
